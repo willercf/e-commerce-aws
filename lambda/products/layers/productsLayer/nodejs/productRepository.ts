833518f490ca4ff4334cd4d6b7735f4a47428750
Willer César de Freitas <willer.freitas@payface.com.br>
2024-06-24 22:09:22 -0300

    add update

--- lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -1,4 +1,3 @@
-import { Product } from "aws-cdk-lib/aws-servicecatalog";
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { v4 as uuid } from "uuid";
 
@@ -65,5 +64,27 @@ export class ProductRepository {
         } else {
             throw new Error("Product Not Found");
         }
+    }
+
+    async update(productId: string, product: Prduct): Promise<Product> {
+
+        const data = await this.ddbClient.update({
+            TableName: this.productsDdb,
+            Key: {
+                id: productId
+            },
+            ConditionExpression: "attribute_exists(id)",
+            ReturnValues: "UPDATED_NEW",
+            UpdateExpression: "set productName = :n, code = :c, price = :p, model = :m",
+            ExpressionAttributeValues: {
+                ":n": product.productName,
+                ":c": product.code,
+                ":p": product.price,
+                ":m": product.model
+            }
+        }).promise();
+
+        data.Attributes!.id = productId;
+        return data.Attributes as Product;
     }
 }
