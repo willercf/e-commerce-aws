fb893975441e7cb1da6df657351b2f4c756938c4
Willer César de Freitas <willer.freitas@payface.com.br>
2024-08-10 10:23:08 -0300

    change product table

--- lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -8,6 +8,7 @@ export interface Product {
     code: string;
     price: number;
     model: string;
+    productUrl: string;
 }
 
 export class ProductRepository {
@@ -75,12 +76,13 @@ export class ProductRepository {
             },
             ConditionExpression: "attribute_exists(id)",
             ReturnValues: "UPDATED_NEW",
-            UpdateExpression: "set productName = :n, code = :c, price = :p, model = :m",
+            UpdateExpression: "set productName = :n, code = :c, price = :p, model = :m, productUrl = :u",
             ExpressionAttributeValues: {
                 ":n": product.productName,
                 ":c": product.code,
                 ":p": product.price,
-                ":m": product.model
+                ":m": product.model,
+                ":u": product.productUrl
             }
         }).promise();
 
