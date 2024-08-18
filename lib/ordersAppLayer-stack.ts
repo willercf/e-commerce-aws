3dd67b3d84ef9edb9cd76ba0821e132cf8baf873
Willer César de Freitas <willer.freitas@payface.com.br>
2024-08-18 10:53:08 -0300

    add orders stack

---
+++ lib/ordersAppLayer-stack.ts
@@ -0,0 +1,27 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as ssm from "aws-cdk-lib/aws-ssm";
+
+export class OrdersAppLayerStack extends cdk.Stack {
+
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+
+        super(scope, id, props);
+        const ordersLayer = new lambda.LayerVersion(
+            this,
+            "OrdersLayer",
+            {
+                code: lambda.Code.fromAsset("lambda/orders/layers/ordersLayer"),
+                compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
+                layerVersionName: "OrdersLayer",
+                removalPolicy: cdk.RemovalPolicy.DESTROY
+            }
+        );
+
+        new ssm.StringParameter(this, "OrdersLayerVersionArn", {
+            parameterName: "OrdersLayerVersionArn",
+            stringValue: ordersLayer.layerVersionArn
+        })
+    }
+}
