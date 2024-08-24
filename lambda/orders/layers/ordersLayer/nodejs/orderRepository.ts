506d21b97c5b1330ebd4685a000d7b51f74b2d2d
Willer César de Freitas <willer.freitas@payface.com.br>
2024-08-23 23:29:59 -0300

    add order repository

--- lambda/orders/layers/ordersLayer/nodejs/orderRepository.ts
+++ lambda/orders/layers/ordersLayer/nodejs/orderRepository.ts
@@ -48,4 +48,50 @@ export class OrderRepository {
         }).promise();
         return result.Items as Order[];
     }
+
+    async findByEmail(email: string): Promise<Order[]> {
+        
+        const result = await this.ddbClient.query({
+            TableName: this.ordersDdb,
+            KeyConditionExpression: "pk = :email",
+            ExpressionAttributeValues: {
+                ":email": email
+            }
+        }).promise();
+        return result.Items as Order[];
+    }
+
+    async findByEmailAndOrderId(email: string, orderId: string): Promise<Order> {
+ 
+        const result = await this.ddbClient.get({
+            TableName: this.ordersDdb,
+            Key: {
+                pk: email,
+                sk: orderId
+            }
+        }).promise();
+
+        if (result.Item) {
+            return result.Item as Order;
+        } else {
+            throw new Error("Order Not Found");
+        }
+    }
+
+    async deleteByEmailAndOrderId(email: string, orderId: string): Promise<Order> {
+
+        const result = await this.ddbClient.delete({
+            TableName: this.ordersDdb,
+            Key: {
+                pk: email,
+                sk: orderId
+            },
+            ReturnValues: "ALL_OLD"
+        }).promise();
+        if (result.Attributes) {
+            return result.Attributes as Order;
+        } else {
+            throw new Error("Order Not Found");
+        }
+    }
 }
